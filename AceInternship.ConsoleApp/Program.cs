ef20dc8fa161fe19e291686926e7a507d7885bc8
Hsu Shwe Yee Myat <emeraldhsu412@gmail.com>
2024-06-16 22:07:35 +0630

    [Complete]Ado DotNet CRUD

---
+++ AceInternship.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using AceInternship.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+
+AdoDotNetCRUD adoDotNetCRUD = new AdoDotNetCRUD();
+
+//adoDotNetCRUD.Read();
+//adoDotNetCRUD.Create("Test Title", "Test Author", "Test Content");
+/*adoDotNetCRUD.Update(2, "Test", "Test", "Test");*/
+adoDotNetCRUD.Delete(10);
