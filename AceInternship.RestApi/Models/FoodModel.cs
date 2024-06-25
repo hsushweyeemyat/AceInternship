26c5a00be6f81f03374025e3eb673319cf0e15db
Hsu Shwe Yee Myat <emeraldhsu412@gmail.com>
2024-06-25 22:34:55 +0630

    [Complete]AdoDotNetWebApi

--- AceInternship.RestApi/Models/FoodModel.cs
+++ AceInternship.RestApi/Models/FoodModel.cs
@@ -12,7 +12,7 @@ public class FoodModel
 {
     [Key]
     public int FoodId { get; set; }
-    public string FoodName { get; set; }
-    public string FoodType { get; set; }
-    public string FoodPrice { get; set; }
+    public string ? FoodName { get; set; }
+    public string ? FoodType { get; set; }
+    public string ? FoodPrice { get; set; }
 }
